930aeb376b3d8b4615fdf75e4e495fab8505120b
anant <anant@307-05.(none)>
2016-09-12 15:01:02 +0530

    add ps1

--- ps1.py
+++ ps1.py
@@ -1,1 +1,13 @@
+from collections import Counter
+from operator import itemgetter
+from itertools import *
+l=list();
+s=raw_input().split()
+for item in (sorted(sorted(Counter(s).items()), key = itemgetter(1), reverse = True)[:3]):
+    print item[0], item[1]
 
+for i in s:
+	k=len(i)
+	for i in permutations(sorted(i),k):
+    		l=i
+	print "".join(l)
